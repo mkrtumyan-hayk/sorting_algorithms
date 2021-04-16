200ad4b92d6598f5674e9a0293b9a030b9e9dd5d
Hayk <68876031+mkrtumyan-hayk@users.noreply.github.com>
2021-04-17 00:54:57 +0400

    Update insertion_sort.cpp

--- Sortings/Sortings/insertion_sort.cpp
+++ Sortings/Sortings/insertion_sort.cpp
@@ -1,17 +1,12 @@
 #include "sortings.h"
 
 void insertion_sort(int* arr, int size) {
-	for (int i = 1; i < size - 1; ++i) {
-		bool swapped = false;
-		for (int j = i; j < size; ++j) {
-			while (arr[j] < arr[j - 1]) {
-				int t = arr[j];
-				arr[j] = arr[j - 1];
-				arr[j - 1] = t;
-				--j;
-				swapped = true;
-			}
-		}
-		if (!swapped) { break; }
-	}
+	for (int j = 1; j < size; ++j) {
+		while (arr[j] < arr[j - 1]) {
+			int t = arr[j];
+			arr[j] = arr[j - 1];
+			arr[j - 1] = t;							 
+			--j;										
+		}											 
+	}												
 }
